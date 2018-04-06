95b464378eeb70dd758917951fa90fe15106957a
winderdancer <brianxiework@hotmail.com>
2018-04-06 00:06:48 -0700

    Create 7.ReverseInteger.cs
    https://leetcode.com/problems/reverse-integer/description/

---
+++ 7.ReverseInteger.cs
@@ -0,0 +1,46 @@
+//https://leetcode.com/problems/reverse-integer/description/
+
+using System;
+
+public class Test
+{
+  public static int Reverse(int x) {
+     
+    int sum = 0;
+    bool isNegative = x < 0;
+    if (isNegative)
+    {
+      x = -x;
+    }
+    
+    
+    while (x > 0)
+    {
+     int digit = x % 10;
+      x = x / 10;
+      
+      try
+      {
+      sum = checked(sum * 10 + digit); 
+      }
+      catch (Exception error)
+      {
+        return 0;
+      }
+      
+    }
+    
+    if (isNegative)
+    {
+      sum = -sum;
+    }
+      
+      return sum;        
+  }
+  
+  public static void Main()
+  {
+
+    Console.WriteLine(Reverse(234));
+  }
+}
